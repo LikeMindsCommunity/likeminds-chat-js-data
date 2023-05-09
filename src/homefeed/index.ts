472c0f9fb52c7b506eb755af1bb5a657fbeed154
Omen Uchiha <omen@LikeMindss-MacBook-Pro.local>
2023-05-09 11:18:06 +0530

    removed onValue function as a return instead added query in return

--- src/homefeed/index.ts
+++ src/homefeed/index.ts
@@ -43,16 +43,7 @@ export class HomeFeedClient extends Base {
     homeFeedListener() {
         const community = JSON.parse(localStorage.getItem('__community__'));
         const query = ref(db, `community/${community.id}`);
-        return onValue(query, (snapshot) => {
-            if (snapshot.exists()) {
-                const snapChatroomId = snapshot.val().chatroom_id;
-                fetch(`${API.FETCH_CHATROOM_HOME}?chatroom_id=${snapChatroomId}`)
-                    .then((res) => res.json())
-                    .then((res: any) => {
-                        return res;
-                    });
-            }
-        });
+        return query;
     }
 
     fbMsg() {
