698df1bac11fbf53296887e2ca53cfa0f53b0fb1
Omen Uchiha <omen@LikeMindss-MacBook-Pro.local>
2023-06-14 23:06:13 +0530

    added builder method

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,36 @@ import { DmFeed } from './pages/directmessage';
 import { Search } from './pages/search';
 import { ExploreFeed } from './pages/explore-feed';
 
-class LMChatClient extends Base {}
+class LMChatClient extends Base {
+    static xApiKey: string;
+    static xPlatformCode: string;
+    static xVersionCode: number;
+    static xSdkSource: string;
+
+    static setApiKey(xapikey: string): SDKBuilder {
+        this.xApiKey = xapikey;
+        return this;
+    }
+
+    static setPlatformCode(xplatformcode: string): SDKBuilder {
+        this.xPlatformCode = xplatformcode;
+        return this;
+    }
+
+    static setVersionCode(xversioncode: number): SDKBuilder {
+        this.xVersionCode = xversioncode;
+        return this;
+    }
+
+    static build() {
+        return new LMChatClient({
+            xApiKey: this.xApiKey,
+            xPlatformCode: this.xPlatformCode,
+            xVersionCode: this.xVersionCode!,
+            xSdkSource: this.xSdkSource,
+        });
+    }
+}
 interface LMChatClient extends HomeFeedClient, ChatroomData, ExploreFeed, Member, DmFeed, Search {}
 
 applyMixins(LMChatClient, [HomeFeedClient, ChatroomData, ExploreFeed, Member, DmFeed, Search]);
