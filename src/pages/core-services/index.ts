2ded22f86d9359ea5fe653423afa8915f19b4667
likeminds <likeminds@likemindss-MacBook-Pro-3.local>
2023-08-17 15:34:18 +0530

    exported core services to make api requests

---
+++ src/pages/core-services/index.ts
@@ -0,0 +1,12 @@
+import NetworkLibrary from 'src/core/services/networklibrary';
+import { Base } from 'src/base';
+import { AxiosRequestConfig } from 'axios';
+
+// CoreServices.ts
+export class CoreServices extends Base {
+    public networkLibrary = new NetworkLibrary();
+
+    async makeAuthenticatedRequest(url: string, config?: AxiosRequestConfig) {
+        return config ? this.networkLibrary.makeAuthenticatedRequest(url, config) : this.networkLibrary.makeAuthenticatedRequest(url);
+    }
+}
