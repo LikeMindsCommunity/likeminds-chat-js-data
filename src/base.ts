1c8fd164aefeac60f82ef2cafd14cf954525b408
aditya-likeminds <141997698+aditya-likeminds@users.noreply.github.com>
2023-10-03 13:43:21 +0530

    Revert "Merge pull request #61 from LikeMindsCommunity/feature/LM-9712_CI_Implementation"
    This reverts commit ce10cfa68e6a058752d23ad0d854ae630ef804b8, reversing
    changes made to b1466c08e3ed2fd0f47d7f6a42b4ef3906ae5fce.

--- src/base.ts
+++ src/base.ts
@@ -1,3 +1,4 @@
+import LMChatClient from 'src';
 import { SdkConfig } from './shared/types';
 import NetworkLibrary from './core/services/networklibrary';
 
@@ -16,5 +17,36 @@ export class Base {
         this.networkLibrary.setApiKey(this.xApiKey);
         this.networkLibrary.setPlatformCode(this.xPlatformCode);
         this.networkLibrary.setVersionCode(this.xVersionCode);
+    }
+}
+
+export class SDKBuilder {
+    xApiKey: string;
+    xPlatformCode: string;
+    xVersionCode: number;
+    xSdkSource: string;
+
+    setApiKey(xapikey: string): SDKBuilder {
+        this.xApiKey = xapikey;
+        return this;
+    }
+
+    setPlatformCode(xplatformcode: string): SDKBuilder {
+        this.xPlatformCode = xplatformcode;
+        return this;
+    }
+
+    setVersionCode(xversioncode: number): SDKBuilder {
+        this.xVersionCode = xversioncode;
+        return this;
+    }
+
+    build() {
+        return new LMChatClient({
+            xApiKey: this.xApiKey,
+            xPlatformCode: this.xPlatformCode,
+            xVersionCode: this.xVersionCode!,
+            xSdkSource: this.xSdkSource,
+        });
     }
 }
