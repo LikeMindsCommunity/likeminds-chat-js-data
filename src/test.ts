c2ac3bf0615fe630a4643e335ae0eead69155cc2
omenUchiha <gaurav.rajput@likeminds.community>
2024-07-12 15:32:26 +0530

    Removed test.ts client testing file

--- src/test.ts
+++
@@ -1,8 +0,0 @@
-import LMChatClient from '.';
-import { LMSDKCallbacks } from './LMCallback';
-
-const lmChatClient = LMChatClient.setLMSDKCallbacks({} as any)
-    .setApiKey('xapikey')
-    .setPlatformCode('xplatformcode')
-    .setVersionCode(1)
-    .build();
