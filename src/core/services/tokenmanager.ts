77917d57a0787b1505caa3bbab24c2df70dd8b60
arnavr15 <arnav.ranjan@likeminds.community>
2024-07-26 19:16:59 +0530

    added checks and exported networkLibrary

--- src/core/services/tokenmanager.ts
+++ src/core/services/tokenmanager.ts
@@ -80,8 +80,10 @@ class TokenManager {
             this.setRefreshToken(accessToken.refresh_token);
             this.setAccessToken(accessToken.access_token);
             // TODO set tokens in local storage
-            localStorage.setItem(TokenValues.LOCAL_ACCESS_TOKEN, accessToken.access_token);
-            localStorage.setItem(TokenValues.LOCAL_REFRESH_TOKEN, accessToken.refresh_token);
+            if (this.xPlatformCode === "rt") {
+                localStorage.setItem(TokenValues.LOCAL_ACCESS_TOKEN, accessToken.access_token);
+                localStorage.setItem(TokenValues.LOCAL_REFRESH_TOKEN, accessToken.refresh_token);
+            }
             this.lmSdkCallback.onAccessTokenExpiredAndRefreshed(this.accessToken, this.refreshToken);
             return accessToken.access_token;
         } catch (error) {
@@ -90,9 +92,10 @@ class TokenManager {
 
             this.setAccessToken(accessToken);
             this.setRefreshToken(refreshToken);
-
-            localStorage.setItem(TokenValues.LOCAL_ACCESS_TOKEN, accessToken);
-            localStorage.setItem(TokenValues.LOCAL_REFRESH_TOKEN, refreshToken);
+            if (this.xPlatformCode === "rt") {
+                localStorage.setItem(TokenValues.LOCAL_ACCESS_TOKEN, accessToken);
+                localStorage.setItem(TokenValues.LOCAL_REFRESH_TOKEN, refreshToken);
+            }
             // this.setRefreshTokenInLocalStorage(refreshToken);
             if (error?.response && error?.response?.status >= 500) throw error;
         }
