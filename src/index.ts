92a642cd2aea2354cb05f01378113439ac0ada4a
sanjaylikeminds <sanjay.kumar@likeminds.community>
2024-09-13 00:25:47 +0530

    filter state message and chatroom topic

--- src/index.ts
+++ src/index.ts
@@ -15,7 +15,7 @@ class SDKBuilder {
     xPlatformCode: string;
     xVersionCode: number;
     xSdkSource: string;
-    excludedConversationStates: any; // Added excludedConversationStates
+    excludedConversationStates: any = null; // Set default value to null
     lmsCallbacks: LMSDKCallbacks | null;
 
     setPlatformCode(xplatformcode: string): SDKBuilder {
@@ -27,6 +27,7 @@ class SDKBuilder {
         this.xVersionCode = xversioncode;
         return this;
     }
+
     setExcludedConversationStates(excludedConversationStates: any): SDKBuilder {
         this.excludedConversationStates = excludedConversationStates;
         return this;
@@ -37,38 +38,41 @@ class SDKBuilder {
             xPlatformCode: this.xPlatformCode,
             xVersionCode: this.xVersionCode!,
             xSdkSource: this.xSdkSource,
-            excludedConversationStates: this.excludedConversationStates,
+            excludedConversationStates: this.excludedConversationStates ?? [], // Ensure a default value if undefined
         });
     }
 }
 
 class LMChatClient extends Base {
     static xPlatformCode: string;
-    static excludedConversationStates: any;
+    static excludedConversationStates: any = []; // Set default to an empty array
     static xVersionCode: number;
     static xSdkSource: string;
     static lmsCallbacks: LMSDKCallbacks | null;
     private giphyApiKey: string = '9hQZNoy1wtM2b1T4BIx8B0Cwjaje3UUR';
+
     public setGipghyApiKey(apiKey: string) {
         this.giphyApiKey = apiKey;
     }
+
     public getGiphyApiKey() {
         return this.giphyApiKey;
     }
+
     public getNetworkLibrary() {
         return this.networkLibrary;
     }
 
     static setLMSDKCallbacks(callback: LMSDKCallbacks) {
         this.lmsCallbacks = callback;
-        // return this;
     }
 
     static setPlatformCode(xplatformcode: string): SDKBuilder {
         this.xPlatformCode = xplatformcode;
         return this;
     }
-    static setExcludedConversationStates(excludedConversationStates: any): SDKBuilder {
+
+    static setExcludedConversationStates(excludedConversationStates: any = []): SDKBuilder {
         this.excludedConversationStates = excludedConversationStates;
         return this;
     }
@@ -80,17 +84,17 @@ class LMChatClient extends Base {
 
     static build() {
         return new LMChatClient({
-            // xApiKey: this.xApiKey,
             xPlatformCode: this.xPlatformCode,
             xVersionCode: this.xVersionCode!,
             xSdkSource: this.xSdkSource,
-            excludedConversationStates: this.excludedConversationStates,
-            // lmCallback: this.lmsCallbacks,
+            excludedConversationStates: this.excludedConversationStates ?? [], // Use default if not set
         });
     }
 }
+
 export default LMChatClient;
 export { LMSDKCallbacks };
+
 interface LMChatClient extends HomeFeedClient, PollClient, ChatroomData, ExploreFeed, Member, DirectMessage, Search, CoreServices {}
 
 applyMixins(LMChatClient, [HomeFeedClient, PollClient, ChatroomData, ExploreFeed, Member, DirectMessage, Search, CoreServices]);
