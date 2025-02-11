6dcec5464d45e58d274fc85937abe48cdb2adac3
shubhbeeplm <shubh.mishra@likeminds.community>
2025-02-11 14:25:06 +0530

    Created PushLogRequest Interface

---
+++ src/shared/enums/severity.ts
@@ -0,0 +1,12 @@
+// LMSeverity Enum
+export enum LMSeverity {
+    INFO = "info",
+    DEBUG = "debug",
+    NOTICE = "notice",
+    WARNING = "warning",
+    ERROR = "error",
+    CRITICAL = "critical",
+    ALERT = "alert",
+    EMERGENCY = "emergency",
+    DEFAULT = "default"
+}
